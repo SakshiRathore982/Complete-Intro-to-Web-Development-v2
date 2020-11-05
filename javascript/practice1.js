24488b685b9c8f13bca94e3b735321195107b4c2
sakshi rathore <65120347+SakshiRathore982@users.noreply.github.com>
2020-11-06 01:29:16 +0530

    JavaScript practice for loop

---
+++ javascript/practice1.js
@@ -0,0 +1,23 @@
+const month=5500;
+const name= "sakshi ";
+const last="rathore";
+const full=name + " " + last;
+const nm=`hello ${name} ${last} how are you !`;
+console.log(nm);
+const sky=5
+if(sky==5)
+{
+    console.log("the sky is blue");
+}
+else
+{
+    console.log("the sky is not their")
+}
+const character='a';
+const times=50;
+let res=" "
+for(let i=0;i<times;i++)
+{ res=res+character;
+}
+console.log(res)
+console.log(res.length)
